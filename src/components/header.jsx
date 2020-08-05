cadeb5f8a01d54114af96fedd55b423d61387184
Danilo Pinheiro <dan.augusto@gmail.com>
2020-08-05 14:56:56 -0300

    Learning React
    Creating TODO task list

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <span className="navbar-brand mb-0 h1">Task List</span>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/components/tasks.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Tasks extends Component {
+  state = {};
+  render() {
+    return <h1>Task List</h1>;
+  }
+}
+
+export default Tasks;
