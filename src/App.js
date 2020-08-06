456f107ff6b51ec0626b2ab813582e22e153aaf6
Danilo Pinheiro <dan.augusto@gmail.com>
2020-08-05 21:06:04 -0300

    Creating tasks component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import Tasks from "./components/tasks";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Tasks />
       <Footer />
     </React.Fragment>
   );
--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -1,9 +1,49 @@
 import React, { Component } from "react";
 
 class Tasks extends Component {
-  state = {};
+  state = {
+    tasks: [
+      {
+        id: 1,
+        title: "Morning workout",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Breakfast",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Rendering project 1",
+        completed: false,
+      },
+    ],
+  };
+
+  handleTaskDone = () => {
+    console.log(this.props);
+  };
+
   render() {
-    return <h1>Task List</h1>;
+    return (
+      <React.Fragment>
+        <ul className="list-group">
+          {this.state.tasks.map((t) => (
+            <li className="list-group-item" key={t.id}>
+              <input
+                onChange={this.handleTaskDone}
+                className="m-2"
+                type="checkbox"
+                aria-label="Task done"
+              ></input>
+              {t.title}
+              <button className="btn btn-sm btn-danger m-2">Delete</button>
+            </li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
   }
 }
 
