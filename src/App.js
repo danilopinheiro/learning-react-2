22393eca5af0d40b8d62c6d293eebb70786f4de6
Danilo Pinheiro <dan.augusto@gmail.com>
2020-08-06 12:34:44 -0300

    Navbar counter and refactoring

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Tasks from "./components/tasks";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Header totalTasks={20} />
-      <main className="container">
-        <Tasks />
-      </main>
-      <Footer />
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    tasks: [
+      {
+        id: 1,
+        title: "Morning workout",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Breakfast",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Rendering project 1",
+        completed: true,
+      },
+    ],
+  };
+
+  handleTaskDone = (task) => {
+    const tasks = [...this.state.tasks];
+    const index = tasks.indexOf(task);
+    tasks[index].completed = !task.completed;
+    this.setState({ tasks: tasks });
+  };
+
+  handleDelete = (task) => {
+    const tasks = this.state.tasks.filter((t) => t.id !== task.id);
+    this.setState({ tasks: tasks });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header
+          totalTasks={
+            this.state.tasks.filter((t) => t.completed === true).length
+          }
+        />
+        <main className="container">
+          <Tasks
+            onTaskDone={this.handleTaskDone}
+            onTaskDelete={this.handleDelete}
+            tasks={this.state.tasks}
+          />
+        </main>
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -2,48 +2,17 @@ import React, { Component } from "react";
 import Task from "./task";
 
 class Tasks extends Component {
-  state = {
-    tasks: [
-      {
-        id: 1,
-        title: "Morning workout",
-        completed: true,
-      },
-      {
-        id: 2,
-        title: "Breakfast",
-        completed: false,
-      },
-      {
-        id: 3,
-        title: "Rendering project 1",
-        completed: true,
-      },
-    ],
-  };
-
-  handleTaskDone = (task) => {
-    const tasks = [...this.state.tasks];
-    const index = tasks.indexOf(task);
-    tasks[index].completed = !task.completed;
-    this.setState({ tasks: tasks });
-  };
-
-  handleDelete = (task) => {
-    const tasks = this.state.tasks.filter((t) => t.id !== task.id);
-    this.setState({ tasks: tasks });
-  };
-
   render() {
+    const { tasks, onTaskDone, onTaskDelete } = this.props;
     return (
       <div>
         <ul className="list-group">
-          {this.state.tasks.map((task) => (
+          {tasks.map((task) => (
             <Task
               key={task.id}
               task={task}
-              onTaskDone={this.handleTaskDone}
-              onTaskDelete={this.handleDelete}
+              onTaskDone={onTaskDone}
+              onTaskDelete={onTaskDelete}
             />
           ))}
         </ul>
