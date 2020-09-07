10f67f03df7973872a72acc2cc34ca88b7149707
danilord <danilopinheiro@users.noreply.github.com>
2020-09-06 21:35:44 -0300

    copyright added to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <nav className="navbar fixed-bottom navbar-dark bg-primary"></nav>;
+  return (
+    <nav className="navbar fixed-bottom navbar-dark bg-primary">
+      <span className="navbar-brand mb-0 h1">Copyright @2020</span>
+    </nav>
+  );
 };
 
 export default Footer;
